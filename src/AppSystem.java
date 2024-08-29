f625c78219abe9a8424ef97d7bce1a4f8bc02a3d
Jaiany Oliveira Silva <131549653+JaianyOS@users.noreply.github.com>
2024-08-29 18:03:29 -0300

    add AppSystem

---
+++ src/AppSystem.java
@@ -0,0 +1,35 @@
+public class AppSystem {
+}
+public class AppSystem {
+
+    private IO io;
+    private SortearPalavras sortearPalavras;
+
+    public AppSystem() {
+        io = new IO();
+        sortearPalavras = new SortearPalavras();
+    }
+
+    public void start() {
+        boolean running = true;
+        while (running) {
+            io.menu();
+            int choice = io.getUserChoice();
+
+            if (choice == 1) {
+                System.out.println("Qual quantidade de palavras deseja sortear? (deve ser 3 ou mais)\n");
+                int quantidade = io.getUserChoice();
+                if (quantidade < 3) {
+                    System.out.println("\nA quantidade deve ser maior ou igual a 3.\n");
+                } else {
+                    sortearPalavras.sorteio(quantidade);
+                }
+            } else if (choice == 2) {
+                running = false;
+            } else {
+                System.out.println("\nOpcao invalida.\n ");
+            }
+        }
+        io.close();
+    }
+}
