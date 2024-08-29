b0095981bd9fccb672219024071359dff44a6985
Jaiany Oliveira Silva <131549653+JaianyOS@users.noreply.github.com>
2024-08-29 17:39:24 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) {
+
+        AppSystem appSystem = new AppSystem();
+        appSystem.start();
+
+
+    }
+}
